904153241bdc9d86dd1cb4eae4718e3f6ab6b757
eventsequor <eventsequor@gmail.com>
2024-04-26 01:34:07 -0500

    [MAIN] performance task1

--- lib/benchmark/performance.ex
+++ lib/benchmark/performance.ex
@@ -1,7 +1,34 @@
 defmodule Benchmark.Performance do
-  def execute(module, fun, args) do
-    start_time = :os.system_time(:millisecond)
+  @time_nano :nanosecond
+  @time_mili :millisecond
+  def execute_nano(module, fun, args) do
+    start_time = :os.system_time(@time_nano)
     Kernel.apply(module, fun, args)
-    :os.system_time(:millisecond) - start_time
+    :os.system_time(@time_nano) - start_time
+  end
+
+  def execute_mili(module, fun, args) do
+    start_time = :os.system_time(@time_mili)
+    _ = Kernel.apply(module, fun, args)
+    :os.system_time(@time_mili) - start_time
+  end
+
+  def average_mili(module, fun, args) do
+    IO.puts("START TEST\n")
+    tries = 10
+
+    total_time =
+      Enum.reduce(1..tries, 0, fn attemp, acc ->
+        IO.puts("Attemp number: #{attemp}")
+        execution_time = execute_mili(module, fun, args)
+        IO.puts("Time of execution in miliseconds: #{execution_time}\n")
+        acc + execution_time
+      end)
+
+    IO.puts("\nRESULTS:")
+    IO.puts("Number of attemps: #{tries}")
+    average_time = total_time / tries
+    IO.puts("Average time of execution: #{average_time}")
+    {:ok}
   end
 end
--- lib/exercises/task1.ex
+++ lib/exercises/task1.ex
@@ -1,6 +1,6 @@
 defmodule Exercises.Task1 do
   def count(string) when is_bitstring(string) do
-    list_of_words =
+    words_list =
       string
       |> String.replace(~r"[?:_!@#$%^&*:|,./]", "")
       |> String.replace("\n", " ")
@@ -9,14 +9,56 @@ defmodule Exercises.Task1 do
       |> String.split(" ")
       |> Enum.filter(fn x -> x != "" end)
 
-    list_of_words = duplicate(list_of_words, 10)
-    count_l(list_of_words)
+    words_list = duplicate(words_list, 14)
+    IO.puts("Number of words #{Integer.to_string(Enum.count(words_list))}")
+    # count_l(list_of_words) #only one list
+
+    # Divide the list int two and sub list the same way n times
+    # split_in_half_parts(words_list, 0)
+
+    # Split in equals parts
+    IO.inspect(split_in_equals_parts(words_list, 12))
+  end
+
+  @spec split_in_equals_parts(any()) :: any()
+  def split_in_equals_parts(words_list, parts_to_divide \\ 1) do
+    parts_to_divide = if parts_to_divide < 1, do: 1, else: parts_to_divide
+
+    sub_list =
+      Enum.chunk_every(words_list, Integer.floor_div(Enum.count(words_list), parts_to_divide))
+
+    x =
+      Enum.map(sub_list, fn part_of_list -> Task.async(fn -> count_l(part_of_list) end) end)
+      |> Task.await_many()
+
+    Enum.reduce(x, Map.new(), fn feature_map, pivot_branch ->
+      Map.merge(feature_map, pivot_branch, fn _k, v1, v2 ->
+        v1 + v2
+      end)
+    end)
+  end
+
+  def split_in_half_parts(words_list, times \\ 0) do
+    if times == 0 do
+      count_l(words_list)
+    else
+      list_of_list =
+        Enum.split(words_list, Integer.floor_div(Enum.count(words_list), 2))
+
+      Enum.map(Tuple.to_list(list_of_list), fn list_splited ->
+        Task.async(fn -> split_in_half_parts(list_splited, times - 1) end)
+      end)
+      |> Task.await_many()
+
+      Enum.reduce(list_of_list, Map.new(), fn feature_map, pivot_branch ->
+        Map.merge(feature_map, pivot_branch, fn _k, v1, v2 ->
+          v1 + v2
+        end)
+      end)
+    end
   end
 
   def count_l(list_of_words) when is_list(list_of_words) do
-    IO.puts("number of words")
-    IO.inspect(Enum.count(list_of_words))
-
     Enum.reduce(list_of_words, Map.new(), fn word, words_map ->
       if Map.get(words_map, word) == nil do
         Map.put(words_map, word, 1)
